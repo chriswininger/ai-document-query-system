d89d0002a6bc482eacc105eab3b45e23e1de9b04
chris <chris@pop-os.localdomain>
2025-03-27 17:48:03 -0400

    translation in place, more names and forms of names added

--- rag-experiments/lemmatization-tests/convert_books_to_nominative_vocabulary_lists.py
+++ rag-experiments/lemmatization-tests/convert_books_to_nominative_vocabulary_lists.py
@@ -2,7 +2,12 @@ import pymorphy2
 import nltk
 import csv
 import os
+import translators as ts
 
+# this seems to be needed to make nltk work
+# these will be cached so once it happened once on a system
+# it won't re-download them
+# https://stackoverflow.com/questions/73744658/resource-punkt-not-found-please-use-the-nltk-downloader-to-obtain-the-resource
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
@@ -11,6 +16,7 @@ nltk.download('punkt_tab')
 def main():
     input_file = './data/harry_potter_book_1.txt'
 
+    # === Get Initial List of Word From Book ===
     print(f"reading in data '{input_file}")
     input_data = read_file(input_file)
 
@@ -25,6 +31,7 @@ def main():
         unique_words_sorted_by_popularity
     )
 
+    # === Normalize the Word Into Nominative Case ===
     morph = pymorphy2.MorphAnalyzer()
     normalized_words = [{ 'word': entry['word'], 'normalized': morph.parse(entry['word'])[0].normal_form, 'count': entry['count'] } for entry in unique_words_sorted_by_popularity]
     normalized_words = list(filter_word_objs_less_than(normalized_words))
@@ -33,6 +40,16 @@ def main():
     save_to_csv(
         './output/' + input_name_to_output_name(input_file, '_nominative_case_unique_words.csv'),
         normalized_words
+    )
+
+    # === Translate the Words ===
+    print('being translated')
+    with_translations = add_translation(normalized_words)
+    print('done translating')
+
+    save_to_csv(
+        './output/' + input_name_to_output_name(input_file, '_final_output.csv'),
+        with_translations
     )
 
     print('done')
@@ -92,8 +109,10 @@ def filter_word_objs_less_than(words, key='normalized', n = 4):
 def filter_names(words):
     # Гарри appears 1366, Рон 321, Гермиона 221
     names = [
-        'огрид',
-        'гермиона',
+        'огрид', 'огрида', 'огриду', 'огридом',
+        'страунс', 'страунса', 'страунсом', 'страунсу', 'страунсе',
+        'гермиона','гермионой', 'гермионы', 'гермионе', 'гермиону',
+        'гриффиндора', 'гриффиндор', 'гриффиндору', 'гриффиндоре',
         'гарри',
         'рон', 'рона',
         'дудли',
@@ -133,6 +152,29 @@ def consolidate_normalized_word(normalized_words_raw):
         key=lambda w: w['count'],
         reverse=True
     )
+
+def add_translation(normalized_words):
+    with_translation = []
+    cnt = 0
+
+    size = len(normalized_words)
+    for entry in normalized_words:
+        word = entry['normalized']
+
+        try:
+            translation = translate_from_russian_to_english(word)
+            with_translation.append({ **entry, 'translation': translation })
+            cnt += 1
+
+            print(f"translated {cnt}/{size}")
+        except Exception as ex:
+            print(f'Error translating word "{word}" at count: ${cnt}: "${ex}"')
+            with_translation.append({**entry, 'translation': 'ERROR' })
+
+    return  with_translation
+
+def translate_from_russian_to_english(word):
+    return ts.server.bing(word, from_language='ru', to_language='en', professional_field='general')
 
 if __name__ == '__main__':
     main()
