04dc047d682411eb5ba3681bfbdd44ce4a4f6230
Chris Wininger <chris.wininger@gmail.com>
2025-04-24 08:49:19 -0400

    iadd ed a db script

---
+++ spring-ai-demo-backend/start-db.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+data_dir="$(pwd)/db-data"
+echo "data_dir: $data_dir"
+
+docker run --name spring-ai-demo-db \
+  --rm \
+  -p 5436:5432 \
+  -v "$data_dir/db":"/var/lib/postgresql/data" \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=xxx \
+  -e POSTGRES_DB=spring-ai-demo-db \
+  -d postgres:17-alpine
